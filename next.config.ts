1b895956510ac574e7f7d4fbb7c68772118669fb
Tobias Halomoan <agyasta1808@gmail.com>
2024-12-20 22:39:22 +0700

    Fix : Added config fixes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config) {
+    // Resolving the '@' alias to the 'src' folder
+    config.resolve.alias["@"] = path.resolve(__dirname, "src");
+    return config;
+  },
+  // Any other Next.js configuration options can be added here.
 };
 
 export default nextConfig;
